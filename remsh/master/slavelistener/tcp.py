b2aa49b3db657acbea1b7bb8b3e6364eb17f9864
Dustin J. Mitchell <dustin@zmanda.com>
2009-05-28 11:56:05 -0400

    let's call it remsh (lower-case 'r')

--- remsh/master/slavelistener/tcp.py
+++ remsh/master/slavelistener/tcp.py
@@ -1,17 +1,17 @@
-# This file is part of Remsh.
+# This file is part of remsh.
 #
-# Remsh is free software: you can redistribute it and/or modify
+# remsh is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# Remsh is distributed in the hope that it will be useful,
+# remsh is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with Remsh.  If not, see <http://www.gnu.org/licenses/>.
+# along with remsh.  If not, see <http://www.gnu.org/licenses/>.
 
 """
 Implements a SlaveListener that listens on a TCP port for incoming connections.
--- setup.py
+++ setup.py
@@ -1,22 +1,22 @@
 #!/usr/bin/env python
-# This file is part of Remsh.
+# This file is part of remsh.
 #
-# Remsh is free software: you can redistribute it and/or modify
+# remsh is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# Remsh is distributed in the hope that it will be useful,
+# remsh is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with Remsh.  If not, see <http://www.gnu.org/licenses/>.
+# along with remsh.  If not, see <http://www.gnu.org/licenses/>.
 
 from setuptools import setup
 
-setup(name='Remsh',
+setup(name='remsh',
       version='1.0',
       description='Parallel remote shell operations in simple Python',
       author='Dustin J. Mitchell',
