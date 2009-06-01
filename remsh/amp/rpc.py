b362ef9aaa85a1368db9f801ac0e3703ad03777c
Dustin J. Mitchell <dustin@v.igoro.us>
2009-05-31 17:49:42 -0700

    add RPC layered atop the Wire classes, with tests

---
+++ remsh/amp/rpc.py
@@ -0,0 +1,114 @@
+# This file is part of remsh.
+#
+# remsh is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# remsh is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with remsh.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+Implements the RPC portions of the AMP protocol, layered atop the wire
+protocol.
+"""
+
+from remsh.amp import wire
+
+Error = wire.Error
+
+class RemoteError(Exception):
+    """An error sent directly from the remote system"""
+
+class RPC(object):
+    """
+    An implementation of the AMP RPC protocol, suitable for subclassing
+    to add remotely-executable methods.  Note that this class assumes that
+    the user knows when to expect an incoming RPC request.
+    """
+
+    def __init__(self, wire):
+        self.wire = wire
+        self.counter = 1
+
+    ##
+    # Public interface
+
+    def call_remote(self, method, **kwargs):
+        """
+	Call the named method on the remote system, passing **kwargs as
+	parameters in the request box.  Returns the response box, with
+	any control key stripped.
+
+	Raises Error for protocol errors or socket.error for network
+	errors or RemoteError for remote errors.
+        """
+        return self._call_remote(method, True, kwargs)
+
+    def call_remote_no_answer(self, method, **kwargs):
+        """
+        Just like call_remote, but do not wait for an answer.
+        """
+        return self._call_remote(method, False, kwargs)
+
+    def handle_call(self):
+        """
+        Handle exactly one RPC invocation from the remote system.
+        """
+        reqbox = self.wire.read_box()
+        if '_ask' not in reqbox:
+            raise Error("request box does not have an _ask key")
+        ask_token = reqbox['_ask']
+        del reqbox['_ask']
+
+        if '_command' not in reqbox:
+            raise Error("request box does not have a _command key")
+        methname = 'remote_' + reqbox['_command']
+        if not hasattr(self, methname):
+            raise Error("unknown command %r" % reqbox['_command'])
+        meth = getattr(self, methname)
+        del reqbox['_command']
+
+        try:
+            respbox = meth(reqbox)
+        except RemoteError, e:
+            respbox = { '_error' : ask_token, '_error_description' : e.args[0], '_error_code' : '0' }
+            self.wire.send_box(respbox)
+            return
+
+        if respbox:
+            respbox['_answer'] = ask_token
+            self.wire.send_box(respbox)
+
+    ##
+    # Implementation
+
+    # TODO: add mutual exclusion here? or at the wire level? or at all?
+    # -> probaby here, because counter is unprotected
+
+    def _call_remote(self, method, expect_answer, arguments):
+        ask = str(self.counter)
+        self.counter += 1
+
+        wirebox = arguments.copy()
+        wirebox['_command'] = method
+        wirebox['_ask'] = ask
+        self.wire.send_box(wirebox)
+
+        if not expect_answer: return
+
+        respbox = self.wire.read_box()
+        if '_answer' in respbox:
+            if respbox['_answer'] != ask: raise Error("answer box does not correspond to the request")
+            del respbox['_answer']
+            return respbox
+        elif '_error' in respbox:
+            if respbox['_error'] != ask: raise Error("error box does not correspond to the request")
+            raise RemoteError(repbox['_error_description'])
+        else:
+            raise Error("response has neither _answer nor _error keys")
---
+++ test/test_amp_rpc.py
@@ -0,0 +1,118 @@
+# This file is part of remsh.
+#
+# remsh is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# remsh is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with remsh.  If not, see <http://www.gnu.org/licenses/>.
+
+import unittest
+import threading
+import socket
+import os
+import time
+
+from remsh.amp import wire, rpc
+
+NAGLE_SLEEP = 0.05
+
+class TestRPCCallsMixin(object):
+    # test invoking RPC methods (call_remote)
+
+    def get_rpc(self, responses):
+        """
+        Get an RPC object set up to communicate with a thread which will respond to
+        each packet with the corresponding box in RESPONSES, remaining silent if that is
+        None.  All boxes received are placed in self.received_boxes.
+        """
+        wire_socket, thread_sock = socket.socketpair()
+        self.received_boxes = []
+
+        def thd():
+            thwire = wire.SimpleWire(thread_sock)
+            for resp in responses:
+                req = thwire.read_box()
+                if not req: break
+                ask = req['_ask']
+                del req['_ask']
+                self.received_boxes.append(req)
+                if resp:
+                    resp = resp.copy()
+                    if '_answer' in resp:
+                        resp['_answer'] = ask
+                    else:
+                        resp['_error'] = ask
+                    thwire.send_box(resp)
+        self.thread = threading.Thread(target=thd)
+        self.thread.setDaemon(1)
+        self.thread.start()
+
+        return rpc.RPC(self.wire_class(wire_socket))
+
+    def stop(self):
+        if self.thread:
+            self.thread.join()
+        self.thread = None
+
+    def setUp(self):
+        self.thread = None
+
+    def tearDown(self):
+        self.stop()
+
+    ## tests
+
+    def test_single_call_remote(self):
+        rpc = self.get_rpc([
+            { '_answer' : None, 'sir' : 'yes, sir' },
+        ])
+        self.failUnlessEqual(rpc.call_remote('jump', howhigh='very'), { 'sir' : 'yes, sir' })
+        self.failUnlessEqual(self.received_boxes, [
+            { '_command' : 'jump', 'howhigh' : 'very' },
+        ])
+
+    def test_multiple_call_remote(self):
+        rpc = self.get_rpc([
+            { '_answer' : None, 'position' : 'standing' },
+            { '_answer' : None, 'position' : 'sitting' },
+        ])
+        self.failUnlessEqual(rpc.call_remote('stand', dir='up'), { 'position' : 'standing' })
+        self.failUnlessEqual(rpc.call_remote('sit', dir='down'), { 'position' : 'sitting' })
+        self.failUnlessEqual(self.received_boxes, [
+            { '_command' : 'stand', 'dir' : 'up' },
+            { '_command' : 'sit', 'dir' : 'down' },
+        ])
+
+    def test_multiple_call_remote_noanswer(self):
+        rpc = self.get_rpc([
+            None,
+            { '_answer' : None, 'position' : 'abdicated' },
+            None,
+            { '_answer' : None, 'position' : 'accepted' },
+        ])
+        self.failUnlessEqual(rpc.call_remote_no_answer('step', dir='around'), None)
+        self.failUnlessEqual(rpc.call_remote('step', dir='down'), { 'position' : 'abdicated' })
+        self.failUnlessEqual(rpc.call_remote_no_answer('step', dir='in'), None)
+        self.failUnlessEqual(rpc.call_remote('step', dir='up'), { 'position' : 'accepted' })
+        self.failUnlessEqual(self.received_boxes, [
+            { '_command' : 'step', 'dir' : 'around' },
+            { '_command' : 'step', 'dir' : 'down' },
+            { '_command' : 'step', 'dir' : 'in' },
+            { '_command' : 'step', 'dir' : 'up' },
+        ])
+
+class TestRPCCallsSimple(TestRPCCallsMixin, unittest.TestCase):
+    wire_class = wire.SimpleWire
+
+class TestRPCCallsResilient(TestRPCCallsMixin, unittest.TestCase):
+    wire_class = wire.ResilientWire
+
+if __name__ == '__main__':
+    unittest.main()
