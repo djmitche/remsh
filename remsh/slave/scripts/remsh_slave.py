580bdbd4bab672e25b445e35304f04c812031f0f
Dustin J. Mitchell <dustin@zmanda.com>
2009-07-05 15:13:45 -0400

    (closes #20) relicense to an MIT-like license

--- remsh/slave/scripts/remsh_slave.py
+++ remsh/slave/scripts/remsh_slave.py
@@ -1,18 +1,6 @@
-#! /usr/bin/python
-# This file is part of Remsh.
-#
-# Remsh is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# Remsh is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with Remsh.  If not, see <http://www.gnu.org/licenses/>.
+# This file is part of remsh
+# Copyright 2009 Dustin J. Mitchell
+# See COPYING for license information
 
 import sys
 import socket
